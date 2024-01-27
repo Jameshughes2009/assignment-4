a163b64c82dd9db86ec2f7b15b67eb2551b36c65
James Hughes <jameshughes2009@hotmail.com>
2024-01-27 10:02:32 -0500

    begining JAVA file

---
+++ v1/Js/script.js
@@ -0,0 +1,35 @@
+var questions = [
+    {
+        questions: "How Many Gods has Kratos Defeated", 
+        answers: [
+            {text: "11", correct: false},
+            {text: "23", correct: true},
+            {text: "19", correct: false},
+            {text: "6", correct: false},
+        ]
+    },
+    {
+        questions: "How Many Gods has Kratos Defeated", 
+        answers: [
+            {text: "11", correct: false},
+            {text: "23", correct: true},
+            {text: "19", correct: false},
+            {text: "6", correct: false},
+        ]
+    },
+    {
+        questions: "How Many Gods has Kratos Defeated", 
+        answers: [
+            {text: "11", correct: false},
+            {text: "23", correct: true},
+            {text: "19", correct: false},
+            {text: "6", correct: false},
+        ]
+    }
+
+];
+var questionsElement = document.getElementById("questions");
+var answerButton = document.getElementById("answer-buttons");
+var nextButton = document.getElementById("next-button");
+
+
